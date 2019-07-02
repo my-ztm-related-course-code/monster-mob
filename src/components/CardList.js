02de9d62f1aced66fc4239d259534bbaeb233227
scotnewbury <scot@scotnewbury.com>
2019-07-02 17:00:13 -0400

    Added ErrorBoundry functionality

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,23 +2,21 @@ import React from 'react';
 import Card from './Card'
 
 const CardList = ({ monsters }) => {
-    return (
-        <div>
-            {
-                monsters.map((user , i) => {
-                    return (
-                        <Card
-                            key={i}
-                            id={monsters[i].id}
-                            name = {monsters[i].name}
-                            email = {monsters[i].email}
-                            phone = {monsters[i].phone}
-                        />
-                    );
-                })
-            }
-        </div>
-    );
+  return (
+    <div>
+      {monsters.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={monsters[i].id}
+            name={monsters[i].name}
+            email={monsters[i].email}
+            phone={monsters[i].phone}
+          />
+        )
+      })}
+    </div>
+  )
 }
 
 export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 class App extends Component {
@@ -37,7 +38,9 @@ class App extends Component {
         <h1 className = 'f1'>Monster Mob</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
         <Scroll>
-          <CardList monsters = {filteredMonsters} />
+          <ErrorBoundry>
+            <CardList monsters = {filteredMonsters} />
+          </ErrorBoundry>
         </Scroll>
       </div>
      ); 
